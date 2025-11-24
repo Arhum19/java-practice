e5d2735db1ddf31d15c16bbff1ec8dc0436c5a47
Arhum19 <arhumnoor10@gmail.com>
2025-11-24 15:37:25 +0500

    taking input in the program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public  class Main {
     public static  void main(String[] args){
-        System.out.println("Arhum noor");
+        String name;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        name = s.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }

3daa4959ee66d80e0073355f8ebaafdc68f223f6
Arhum19 <arhumnoor10@gmail.com>
2025-11-24 17:34:56 +0500

    array reverse

---
+++ src/array.java
@@ -0,0 +1,22 @@
+public class array {
+    public static void main(String[] args){
+//        int[] arr = {5, 2, 8, 1, 3};
+//        for(int i = 0; i<= arr.length - 1; i++){
+//            System.out.println("Element at index " + i + ": " + arr[i]);
+//        }
+//        String[] names = {"Alice", "Bob", "Charlie"};
+//        for(int i = 0; i < names.length; i++) {
+//            System.out.println("Name at index " + i + ": " + names[i]);
+//        }
+        int[] numbers = {34, 12, 5, 67, 23};
+//        for(int i = 0; i < numbers.length; i++){
+//            System.out.println("Element at index " + i + ": " + numbers[i]);
+//        }
+//        System.out.println("Reversed Array:");
+//        for(int i = numbers.length -1 ; i >= 0; i--){
+//            System.out.println(numbers[i]);
+//
+//        }
+
+    }
+}

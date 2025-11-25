903ea59493a88faf9884db72887e22b0b5977b52
Arhum19 <arhumnoor10@gmail.com>
2025-11-25 13:01:31 +0500

    upate wordcount file

--- day2/src/wordcout.java
+++ day2/src/wordcout.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
 public class wordcout {
+    public static void main(String[] args) {
+        String str = "My name is Muhammad Arhum";
+        HashMap<String, Integer> count = new HashMap<>();
+        for (String word : str.split(" ")){
+            word = word.toLowerCase();
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        for (Map.Entry<String, Integer> entry : count.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
 }
